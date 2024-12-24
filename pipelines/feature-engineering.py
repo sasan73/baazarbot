168cb00f3062789f35ce828f81b2eab15e579faf
Sassan Owssia <sassan.owssia@gmail.com>
2024-12-24 11:54:52 +0100

    feat: Add feature-engineering pipeline

---
+++ pipelines/feature-engineering.py
@@ -0,0 +1,13 @@
+from zenml import pipeline
+
+from steps import feature_engineering
+
+@pipeline
+def feature_engineering() -> List[str]:
+    raw_documents = feature_engineering.get_data_warehouse()
+    cleaned_documents = feature_engineering.clean_documents(raw_documents)
+    cleaning_step = feature_engineering.load_to_vectore_db(cleaned_documents)
+    
+    embedded_documents = feature_engineering.chunk_and_embed(cleaned_documents)
+    embedding_step = feature_engineering.load_to_vectore_db(embedded_documents)
+    return [cleaning_step.invocaiton_id, embedded_documents.invocaiton_id]
