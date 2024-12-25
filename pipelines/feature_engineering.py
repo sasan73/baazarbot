bd65c72430563d8d4b7cc96261427df7deebc887
Sassan Owssia <sassan.owssia@gmail.com>
2024-12-25 16:26:03 +0100

    bug: Fix ecoiran crawler to include data category during Document creation

--- pipelines/feature_engineering.py
+++ pipelines/feature_engineering.py
@@ -7,7 +7,7 @@ def feature_engineering() -> List[str]:
     raw_documents = feature_engineering.get_data_warehouse()
     cleaned_documents = feature_engineering.clean_documents(raw_documents)
     cleaning_step = feature_engineering.load_to_vectore_db(cleaned_documents)
-    
+
     embedded_documents = feature_engineering.chunk_and_embed(cleaned_documents)
     embedding_step = feature_engineering.load_to_vectore_db(embedded_documents)
     return [cleaning_step.invocaiton_id, embedded_documents.invocaiton_id]
