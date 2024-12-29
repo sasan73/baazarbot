2966fe52f9899fb41376e5d65e60c07020e25e81
Sassan Owssia <sassan.owssia@gmail.com>
2024-12-29 19:08:58 +0100

    bug: fix feature engineering steps.

--- steps/feature_engineering/__init__.py
+++ steps/feature_engineering/__init__.py
@@ -1,11 +1,11 @@
 from steps.feature_engineering.clean import clean_documents
-from steps.feature_engineering.load_vectoredb import load_to_vectore_db
+from steps.feature_engineering.load_vectordb import load_to_vector_db
 from steps.feature_engineering.get_data_warehouse import get_data_warehouse
 from steps.feature_engineering.rag import chunk_and_embed
 
 __all__=[
     "clean_documents",
-    "load_to_vectore_db",
+    "load_to_vector_db",
     "get_data_warehouse",
     "chunk_and_embed",
 ]
--- steps/feature_engineering/rag.py
+++ steps/feature_engineering/rag.py
@@ -3,6 +3,8 @@ from typing import Annotated, List
 from zenml import get_step_context, step
 
 from baazarbot.application.preprocessing import ChunkingDispatcher, EmbeddingDispatcher
+from baazarbot.application import utils
+from baazarbot.domain.cleaned_documents import CleanedDocument
 from baazarbot.domain.chunks import Chunk 
 from baazarbot.domain.embedded_chunks import EmbeddedChunk
 
@@ -10,8 +12,8 @@ from baazarbot.domain.embedded_chunks import EmbeddedChunk
 @step
 def chunk_and_embed(
     cleaned_documents: Annotated[List[CleanedDocument], "cleaned_documents"],
-) -> Annotated[List[EmbeddedChunk], "embedded_chunks"]
-    metadata = {"chunking: {}", "embedding": {}, "num_documents": len(cleaned_documents)}
+) -> Annotated[List[EmbeddedChunk], "embedded_chunks"]:
+    metadata = {"chunking":{}, "embedding": {}, "num_documents": len(cleaned_documents)}
     
     embedded_chunks = []
     for document in cleaned_documents:
@@ -20,14 +22,14 @@ def chunk_and_embed(
         
         for batched_chunks in utils.misc.batch(chunks, 10):
             batched_embedded_chunks = EmbeddingDispatcher.dispatch(batched_chunks)
-            embedded_chunks.extend(batched_chunks)
+            embedded_chunks.extend(batched_embedded_chunks)
         
     metadata["embedding"] = _add_embedding_metadata(embedded_chunks, metadata["embedding"])
     metadata["num_chunks"] = len(embedded_chunks)
     metadata["num_embedded_chunks"] = len(embedded_chunks)
     
     step_context = get_step_context()
-    step_context.add_output_metadata(output_name="embedded_documents", metadata=metadata)
+    step_context.add_output_metadata(output_name="embedded_chunks", metadata=metadata)
     return embedded_chunks
 
 def _add_chunks_metadata(chunks: list[Chunk], metadata: dict) -> dict:
@@ -41,7 +43,7 @@ def _add_chunks_metadata(chunks: list[Chunk], metadata: dict) -> dict:
     return metadata
 
 # add metadata if needed as of now no metadata.
-def _add_embeddings_metadata(embedded_chunks: list[EmbeddedChunk], metadata: dict) -> dict:
+def _add_embedding_metadata(embedded_chunks: list[EmbeddedChunk], metadata: dict) -> dict:
     for embedded_chunk in embedded_chunks:
         category = embedded_chunk.get_category()
         if category not in metadata:
