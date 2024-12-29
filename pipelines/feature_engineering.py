88db880e341cb34ef4a410e851581c614ef11f32
Sassan Owssia <sassan.owssia@gmail.com>
2024-12-29 19:09:33 +0100

    bug: fix zenml pipeline bugs

--- pipelines/feature_engineering.py
+++ pipelines/feature_engineering.py
@@ -1,13 +1,13 @@
 from zenml import pipeline
 
-from steps import feature_engineering
+from steps import feature_engineering as fe_steps
 
 @pipeline
-def feature_engineering() -> List[str]:
-    raw_documents = feature_engineering.get_data_warehouse()
-    cleaned_documents = feature_engineering.clean_documents(raw_documents)
-    cleaning_step = feature_engineering.load_to_vectore_db(cleaned_documents)
+def feature_engineering() -> list[str]:
+    raw_documents = fe_steps.get_data_warehouse()
+    cleaned_documents = fe_steps.clean_documents(raw_documents)
+    cleaning_step = fe_steps.load_to_vector_db(cleaned_documents)
 
-    embedded_documents = feature_engineering.chunk_and_embed(cleaned_documents)
-    embedding_step = feature_engineering.load_to_vectore_db(embedded_documents)
-    return [cleaning_step.invocaiton_id, embedded_documents.invocaiton_id]
+    embedded_documents = fe_steps.chunk_and_embed(cleaned_documents)
+    embedding_step = fe_steps.load_to_vector_db(embedded_documents)
+    return [cleaning_step.invocation_id, embedded_documents.invocation_id]
