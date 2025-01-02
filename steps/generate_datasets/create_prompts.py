e8592089b56f3f6e32426efb3266754ec1ea552d
Sassan Owssia <sassan.owssia@gmail.com>
2025-01-02 21:55:17 +0100

    feat: Add prompt creation zenml step

---
+++ steps/generate_datasets/create_prompts.py
@@ -0,0 +1,30 @@
+from zenml import step, get_step_context
+
+from baazarbot.application.dataset.generation import get_dataset_generator
+from baazarbot.domain.dataset import DatasetType
+from baazarbot.domain.cleaned_documents import CleanedDocument
+from baazarbot.domain.prompt import GenerateDatasetSamplesPrompt
+
+@step
+def create_prompts(
+    documents: list[CleanedDocument],
+    dataset_type: DatasetType,
+) -> dict[DataCategory, list[GenerateDatasetSamplesPrompt]]:
+    generator = get_dataset_generator(dataset_type)
+    prompts = generator.get_prompts(documents)
+    
+    step_context = get_step_context()
+    step_context.add_output_metadata(output_name="prompts", _get_prompt_metadata(prompts))
+    
+    return prompts
+
+def get_prompt_metadata(category_prompts: dict) -> dict:
+    metadata = {}
+    for category, prompts in category_prompts.items():
+        if category not in metadata:
+            metadata[category] = {}
+        metadata[category]["number_of_prompts"] = len(prompts)
+        token_lengths.append(prompts.num_tokens)
+    metadata[category]["average_prompt_token_length"] = sum(token_lengths) / len(token_lengths)
+
+    return metadata
