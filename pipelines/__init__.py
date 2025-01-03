5bca1f380d2580f5fd9eaa91644b6e480aba84ce
smbuser sassan.owssia <sassan.owssia@blnrg024.ppmd.siemens.net>
2025-01-04 00:10:00 +0100

    feat: Add dataset generation tool run function

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,6 +1,8 @@
 from .digital_data_etl import digital_data_etl
 from .feature_engineering import feature_engineering
+from .generate_datasets import generate_datasets
 __all__ = [
     "digital_data_etl",
     "feature_engineering",
+    "generate_datasets",
 ]
--- tools/run.py
+++ tools/run.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import click
 
 from baazarbot import settings
-from pipelines import digital_data_etl, feature_engineering
+from pipelines import digital_data_etl, feature_engineering, generate_datasets
 
 @click.command()
 @click.option("--no-cache", is_flag=True, default=False, help="Option to disable cache.")
@@ -12,10 +12,12 @@ from pipelines import digital_data_etl, feature_engineering
 @click.option("--run-feature-engineering", is_flag=True, default=False, help="Option to run feature engineering.")
 @click.option("--export_settings", is_flag=True, default=False, help="Option to export settings to ZenML secrets.")
 @click.option("--etl-config-filename", default="digital_data_etl.yml", help="ETL configuration filename.")
+@click.option("--run_generate_instruct_datasets", is_flag=True, default=False, help="Option to run generate instruction dataset.")
 def main(
     no_cache: bool,
     run_etl: bool,
     run_feature_engineering: bool,
+    run_generate_instruct_datasets: bool,
     export_settings: bool,
     etl_config_filename: str = "digital_data_etl.yml",
 ) -> None:
@@ -48,6 +50,12 @@ def main(
         # pipeline_args["config_path"] = root_dir / "configs" / "feature_engineering.yaml"
         pipeline_args["run_name"] = f"digital_data_fe_run_{dt.now().strftime('%Y_%m_%d_%H_%M_%S')}"
         feature_engineering.with_options(**pipeline_args)(**run_args_fe)
+
+    if run_generate_instruct_datasets:
+        run_args_cd = {}
+        pipeline_args["config_path"] = root_dir / "configs" / "generate_instruct_datasets.yaml"
+        pipeline_args["run_name"] = f"generate_instruct_datasets_run_{dt.now().strftime('%Y_%m_%d_%H_%M_%S')}"
+        generate_datasets.with_options(**pipeline_args)(**run_args_cd)
 
 if __name__ == "__main__":
     main()
